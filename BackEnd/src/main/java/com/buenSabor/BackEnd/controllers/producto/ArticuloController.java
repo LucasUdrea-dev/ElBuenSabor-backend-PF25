373a2693f2571303ee3c6f56a6ea90d1e80c9396
LuisOrtizElizondo <luisg.ortiz@alumnos.frm.utn.edu.ar>
2025-06-12 18:12:13 -0300

    Eliminado codigo basura

--- BackEnd/src/main/java/com/buenSabor/BackEnd/controllers/producto/ArticuloController.java
+++ BackEnd/src/main/java/com/buenSabor/BackEnd/controllers/producto/ArticuloController.java
@@ -34,35 +34,7 @@ public class ArticuloController extends BeanControllerImpl<Articulo,ArticuloServ
 
     @Autowired
     private ArticuloMapper articuloMapper;
-
-    /*@Operation(summary = "Guardar un nuevo artículo (insumo o manufacturado) a partir de un DTO")
-    @Schema( description = "Puede ser un ArticuloInsumoDTO o ArticuloManufacturadoDTO"
-            ,oneOf = {InsumoDTO.class, ArticuloManufacturadoDTO.class})
-    @PostMapping("/crear")
-    public ResponseEntity<?> saveFromDTO(@RequestBody ArticuloDTO dto) {
-        try {
-            Articulo articulo = articuloMapper.toArticulo(dto);
-            return ResponseEntity.status(HttpStatus.OK).body(articuloService.guardar(articulo));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("{\"error\":\"Error al guardar el artículo: " + e.getMessage() + "\", " +
-                          "\"causa\":\"" + (e.getCause() != null ? e.getCause().getMessage() : "No hay causa específica") + "\"}");
-        }
-    }
     
-    @Operation(summary = "Actualizar un artículo existente DTO")
-    @PutMapping("/actualizar/{id}")
-    public ResponseEntity<?> updateArticuloDTO (@PathVariable Long id, @RequestBody ArticuloDTO dto) {
-        try {
-            Articulo articulo = articuloMapper.toArticulo(dto);
-            return ResponseEntity.status(HttpStatus.OK).body(articuloService.actualizar(id, articulo));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("{\"error\":\"Error al actualizar el artículo: " + e.getMessage() + "\", " +
-                          "\"causa\":\"" + (e.getCause() != null ? e.getCause().getMessage() : "No hay causa específica") + "\"}");
-        }
-    }*/
-
     @Operation(summary = "Obtener artículos que existen")
     @GetMapping("/existentes")
     public ResponseEntity<?> getArticulosExistentes() {
