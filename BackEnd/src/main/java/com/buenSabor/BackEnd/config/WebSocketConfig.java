eb543b75ebf229caa827d9ffb5b5111010dd0312
Tomas <tomasloha@gmail.com>
2025-12-05 23:35:40 -0300

    algo

--- BackEnd/src/main/java/com/buenSabor/BackEnd/config/WebSocketConfig.java
+++ BackEnd/src/main/java/com/buenSabor/BackEnd/config/WebSocketConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+//something more
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
 
